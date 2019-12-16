6f5df4e26f0ff102a282cfd489281fd15fd7981e
Philip <philip.grach@gmail.com>
2019-12-16 14:50:16 +0200

    DayTime
    * DayTime utility class added

---
+++ Assets/Scripts/Util/DayTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayTime : MonoBehaviour
+{
+    public static int GetHours()
+    {
+        return System.DateTime.Now.Hour;
+    }
+
+    public static int GetMinutes()
+    {
+        return System.DateTime.Now.Minute;
+    }
+
+}
